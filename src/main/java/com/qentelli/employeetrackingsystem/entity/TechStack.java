9a2746718a2fce3536554f07adec02a9f3b49ce6
Yellapu Anil Kumar <anil.yellapu@qentelli.com>
2025-07-02 12:21:08 +0530

    updating tech stack

--- src/main/java/com/qentelli/employeetrackingsystem/entity/TechStack.java
+++ src/main/java/com/qentelli/employeetrackingsystem/entity/TechStack.java
@@ -1,11 +1,8 @@
 package com.qentelli.employeetrackingsystem.entity;
 
 public enum TechStack {
-    JAVA,
-    PYTHON,
-    REACT,
-    ANGULAR,
-    SPRING_BOOT,
-    NODE_JS,
-    DOTNET
+	Frontend,
+	Backend,
+	FullStack,
+	Testing
 }
